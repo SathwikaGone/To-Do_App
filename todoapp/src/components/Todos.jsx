0f394bea8f8554e4617def620a743fe4b21f03ae
SathwikaGone <gonesathwika@gmail.com>
2020-02-04 10:25:54 -0500

    committing Todo home and about

---
+++ todoapp/src/components/Todos.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import propTypes from "prop-types";
+
+class Todos extends Component {
+  state = {};
+
+  render() {
+    // console.log(this.props.todos);
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        deleteItem={this.props.deleteItem}
+      />
+    ));
+  }
+}
+//proptypes
+Todos.propTypes = {
+  todos: propTypes.array.isRequired
+};
+
+export default Todos;
